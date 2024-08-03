9070f549c783907822621ab8c0441e20a74291a5
itsadi-24 <158060025+itsadi-24@users.noreply.github.com>
2024-08-03 15:17:12 +0530

    "Added dotenv import and config, and connected to database on server startup"

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
+import dotenv from 'dotenv';
 import express from 'express';
+import { connectDB } from './config/db.js';
 
+dotenv.config();
 const app = express();
 
 app.listen(5000, () => {
+  connectDB();
   console.log('Server is running on port 5000');
 });
